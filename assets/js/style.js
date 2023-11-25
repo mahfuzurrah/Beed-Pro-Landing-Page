711f66705721a8bc0b130559a6eaa129d22ed6d0
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-11-25 18:47:53 +0600

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  /* ======================= Navbar ======================== */
+
+  var navLinks = document.querySelectorAll(".nav_link");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navLinks.forEach(function (navLink) {
+        navLink.classList.remove("active");
+      });
+
+      this.classList.add("active");
+    });
+  });
+
+  const toggleButton = document.getElementById("toggleButton");
+  const toogle_menu = document.getElementById("toogle_menu");
+
+  toggleButton.addEventListener("click", function () {
+    this.classList.toggle("open");
+    toogle_menu.classList.toggle("open", this.classList.contains("open"));
+  });
+
+  /* ======================= Swiper ======================== */
+
+  var swiper = new Swiper(".mySwiper", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  var swiper = new Swiper(".Swiper_product_overview", {
+    // direction: 'horizontal',
+    // loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+});
